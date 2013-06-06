a9fc2ef362a307adf1ec614d6eb11a5a4d69b2ba
Michael Volovar <mike@volovar.com>
2013-06-05 22:54:34 -0500

    Additional navigation work. Supports keyboard and touch. Updated styling, minor changes to header markup.

--- js/navigation.js
+++ js/navigation.js
@@ -1,43 +1,63 @@
 YUI().use(['node', 'node-style', 'event', 'event-tap'], function(Y) {
     var navButton = Y.one('#navButton'),
         navHeader = Y.one('#navigationHeader'),
+        buttonHeight = navButton.get('offsetHeight'),
         body = Y.one('body');
 
-    function addNavHeight(heightAdd, display, addRemove) {
+    function addNavHeight(heightAdd, addRemove) {
         navHeader.setStyle('height', heightAdd);
 
         if(addRemove) {
             navHeader.addClass('open');
             navButton.addClass('active');
+            body.setStyle('overflow', 'hidden');
         } else {
             navHeader.removeClass('open');
             navButton.removeClass('active');
+            body.setStyle('overflow', 'auto');
         }
-
-        body.setStyle('overflow', display);
     }
 
-    navButton.on('tap', function(e) {
+    navButton.on(['tap', 'keypress'], function(e) {
         var windowHeight = Y.one('window').get('winHeight'),
+            navHeight = navHeader.getStyle('height'),
             bodyHeight = body.get('offsetHeight'),
-            navHeight = navHeader.getStyle('height');
+            pressed = e.keyCode;
 
-        if(navHeight == '70px' && windowHeight > bodyHeight) {
-            addNavHeight(windowHeight, 'hidden', true);
-        } else if(navHeight == '70px' && windowHeight < bodyHeight) {
-            addNavHeight(bodyHeight, 'hidden', true);
+        Y.log(pressed);
+
+        if(navHeight == '0px' && windowHeight > bodyHeight) {
+            addNavHeight(windowHeight - buttonHeight, true);
+        } else if(navHeight == '0px' && windowHeight < bodyHeight) {
+            addNavHeight(bodyHeight, true);
         } else {
-            addNavHeight('70px', 'auto', false);
+            addNavHeight('0px', false);
         }
 
         e.stopPropagation();
+    });
+
+    navButton.on('mouseenter', function() {
+        this.addClass('hover');
+    });
+
+    navButton.on('mouseleave', function() {
+        this.removeClass('hover');
+    });
+
+    Y.one('body').on('touchmove', function(){
+        navButton.removeClass('hover');
     });
 
     Y.one('window').on('resize', function() {
         var windowHeight = Y.one('window').get('winHeight');
 
         if(navHeader.hasClass('open')) {
-            navHeader.setStyle('height', windowHeight);
+            navHeader.setStyle('height', windowHeight - buttonHeight);
         }
+    });
+
+    Y.one('.nav').on('focus', function(e) {
+        addNavHeight(Y.one('window').get('winHeight'), true);
     });
 });
