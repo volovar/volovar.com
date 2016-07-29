bbabf6a2b48612979ffa9521ed708d9e70db09f5
Michael Volovar <michael@Michaels-MacBook-Air.local>
2016-07-29 03:24:26 -0700

    updates to resume, changes to js files

--- public/js/colorizer.js
+++ public/js/colorizer.js
@@ -1,51 +1,55 @@
-var elements = document.getElementsByClassName('js-colorized');
-var homeLink = document.getElementsByClassName('home-link');
-var colorSet = ['#aab5b5', '#569ccb', '#b864a1', '#4cabab', '#7b4cab', '#33afc4', '#f17ad1'];
-var colorCount = 0;
-var colorInterval;
-var blockColor;
+document.addEventListener("DOMContentLoaded", function (event) {
+    (function () {
+        var elements = document.getElementsByClassName('js-colorized');
+        var homeLink = document.getElementsByClassName('home-link');
+        var colorSet = ['#aab5b5', '#569ccb', '#b864a1', '#4cabab', '#7b4cab', '#33afc4', '#f17ad1'];
+        var colorCount = 0;
+        var colorInterval;
+        var blockColor;
 
-darkenColors();
-setColorChange();
-
-function randomizeColor(min, max) {
-    return Math.floor(Math.random() * (max - min + 1));
-};
-
-// shadeColor courtesy http://stackoverflow.com/a/13542669
-function shadeColor(color, percent) {
-    var f = parseInt(color.slice(1),16),
-        t = percent<0?0:255,
-        p = percent<0?percent*-1:percent,
-        R = f>>16,
-        G = f>>8&0x00FF,
-        B = f&0x0000FF;
-    return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
-};
-
-function setColorChange() {
-    var colorInterval = setInterval(function () {
         darkenColors();
-    }, 2200);
-}
+        setColorChange();
 
-function darkenColors() {
-    for (var i = 0; i < (elements.length); i++) {
-        blockColor = shadeColor(colorSet[colorCount], (i * -0.11));
-        setDelay(i, blockColor);
-    };
+        function randomizeColor(min, max) {
+            return Math.floor(Math.random() * (max - min + 1));
+        };
 
-    if (colorCount >= (colorSet.length - 1)) {
-        colorCount = 0;
-    } else {
-        colorCount += 1;
-    }
-};
+        // shadeColor courtesy http://stackoverflow.com/a/13542669
+        function shadeColor(color, percent) {
+            var f = parseInt(color.slice(1),16),
+                t = percent<0?0:255,
+                p = percent<0?percent*-1:percent,
+                R = f>>16,
+                G = f>>8&0x00FF,
+                B = f&0x0000FF;
+            return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
+        };
 
-function setDelay(i, color) {
-    clearTimeout(colorTimer);
+        function setColorChange() {
+            var colorInterval = setInterval(function () {
+                darkenColors();
+            }, 2200);
+        }
 
-    var colorTimer = setTimeout(function() {
-        elements[i].style.background = color;
-    }, (i * 220));
-}
+        function darkenColors() {
+            for (var i = 0; i < (elements.length); i++) {
+                blockColor = shadeColor(colorSet[colorCount], (i * -0.11));
+                setDelay(i, blockColor);
+            };
+
+            if (colorCount >= (colorSet.length - 1)) {
+                colorCount = 0;
+            } else {
+                colorCount += 1;
+            }
+        };
+
+        function setDelay(i, color) {
+            clearTimeout(colorTimer);
+
+            var colorTimer = setTimeout(function() {
+                elements[i].style.background = color;
+            }, (i * 220));
+        }
+    })();
+});
--- public/js/date.js
+++ public/js/date.js
@@ -1,7 +1,11 @@
-var today = new Date(),
-    year = today.getFullYear(),
-    elem = document.getElementById('date');
+document.addEventListener("DOMContentLoaded", function(event) {
+    (function () {
+        var today = new Date();
+        var year = today.getFullYear();
+        var elem = document.getElementById('date');
 
-if(elem) {
-    elem.innerHTML = year;
-}
+        if(elem) {
+            elem.textContent = year;
+        }
+    })();
+});
