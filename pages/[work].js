6e38beb896deab3f6c4950cca44d2d7e703ab4a6
volovar <michaelvolovar@gmail.com>
2019-08-21 00:57:30 -0400

    [VLVR | AUG-21-002] Refactor Work Page
    Removed some unused code

--- pages/[work].js
+++ pages/[work].js
@@ -1,22 +1,6 @@
 import React, { Component } from "react";
 import Layout from "../src/components/Layout";
 
-class Index extends Component {
-    static async getInitialProps({ query }) {
-        return { ...query };
-    }
-
-    render() {
-        console.log(this.props);
-        const { id } = this.props;
-        return (
-            <Layout>
-                <div>{id}</div>
-            </Layout>
-        );
-    }
-}
-
 const Work = ({ name }) => (
     <Layout>
         <div>{name}</div>
